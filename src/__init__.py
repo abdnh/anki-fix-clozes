448f9f6f502acf479b8afcf0b567bef878934adb
Abdo <abdo@abdnh.net>
2023-09-13 16:54:45 +0300

    Avoid running GUI code in tests

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,4 @@
-import functools
+import sys
 
-from aqt import gui_hooks, mw
-from aqt.browser import Browser
-from aqt.qt import *
-
-from .gui.field_selector import FieldSelectionDialog
-
-
-def on_fix_clozes(browser: Browser) -> None:
-    if not browser.selected_notes():
-        return
-    dialog = FieldSelectionDialog(mw, browser, browser.selected_notes())
-    dialog.open()
-
-
-def add_browser_menu(browser: Browser) -> None:
-    action = QAction("Fix Clozes", browser)
-    qconnect(action.triggered, functools.partial(on_fix_clozes, browser=browser))
-    browser.form.menu_Notes.addAction(action)
-
-
-gui_hooks.browser_menus_did_init.append(add_browser_menu)
+if "pytest" not in sys.modules:
+    from . import hooks
